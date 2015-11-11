897499a7caa1d6535aa9b5eb40e266a63b2a2d2c
Jorge Reig <reig.aguado@gmail.com>
2015-11-11 19:50:09 +0100

    Intento para guardar las invitaciones en memoria. no funciona

--- app/routes.rb
+++ app/routes.rb
@@ -1,14 +1,33 @@
 require 'sinatra'
 require 'shotgun'
 class Invitacion
+	@@index = 0
+	@@instances = []
 	attr_accessor :tema, :content
+
+	def self.save instance
+		@@instances.push instance
+		@@index = @@index + 1
+	end
+
+	def self.retrieve index
+		@@instances[index]
+	end
+
+	def self.show_all 
+		p @@instances
+	end
 end
+
+
 
 def show_content(params)
 	@invitacion = Invitacion.new
 	@invitacion.tema = params[:tema]
 	@invitacion.content = params[:content]
 	@title = @invitacion.tema
+	Invitacion.save(@invitacion)
+	Invitacion.show_all
 	@fullpath = request.fullpath
 	set_url(@invitacion.tema, @invitacion.content)
 	erb :tema
@@ -34,4 +53,8 @@ post '/tema' do
 end
 get '/tema' do
 	show_content(params)
+end
+get '/tema_by_id' do
+	Invitacion.show_all
+	p Invitacion.retrieve params[:index].to_i
 end
